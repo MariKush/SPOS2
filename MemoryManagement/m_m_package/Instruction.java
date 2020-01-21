5cabebb2cbf46775b5d974aa6488b10de3b04c09
MariKush <43176370+MariKush@users.noreply.github.com>
2020-01-21 20:59:09 +0200

    work program

---
+++ MemoryManagement/m_m_package/Instruction.java
@@ -0,0 +1,14 @@
+package m_m_package;
+
+public class Instruction 
+{
+  public String inst;
+  public long addr;
+
+  public Instruction( String inst, long addr ) 
+  {
+    this.inst = inst;
+    this.addr = addr;
+  } 	
+
+}
---
+++ MemoryManagement/start.bat
@@ -0,0 +1,5 @@
+cd m_m_package
+javac -nowarn *.java
+cd ..
+java m_m_package.MemoryManagement commands memory.conf
+pause
